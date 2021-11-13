5d2feafd447fc52c52bd498a72507c4b8f59abed
Daneil Benedict L. Omoto <dbomoto@gmail.com>
2021-11-13 09:53:58 +0000

    Added tailwind auto complete and a few code refactoring.

--- README.md
+++ README.md
@@ -1,30 +1,21 @@
 # Tailwind-Editor
 ## HTML live editor with Tailwind CSS
 
-##INSTRUCTIONS
+### HOW TO USE
 - RUN button > use this when you have a custom value for tailwind classes.
+- COMPILE AUTO/OFF > user preference if they want to auto compile or not.
+- VIM Keys ON/OFF > use vim key bindings
+- Open in New Tab > open in new tab the preview page
 
-## GOALS
-- on each entry on code editor, it will make a request to the server, compile the new css of tailwind, and send the new html to the client iframe via ejs for easier injection of code.
-- or just use ejs for all html.
+### GOALS
+- Free to use.
+- Unsplash API.
+- Buy me a coffee.
+- Advertise that i am looking for a job.
 
-- "open in new tab" function.
-
-- communications with the server is only for saving uploaded images to image hosting site.
-
-- add Unsplash api for easier image resource acquisition.
-
-- login feature via replit,fb,google,password.
-
-- each user can save their webpage on the server.
-
-- buy me a coffee after login
-
-##DEVELOPER NOTES
-- ORIGINAL SOURCE: https://reactgo.com/build-your-own-html-editor/
-
-- Loading css on iframe proved to be very difficult, I opted to use as a div wrapper for the simulated webpage.
+### DEVELOPER NOTES
+- ORIGINAL SOURCE FOR REFERENCE: https://reactgo.com/build-your-own-html-editor/
 
 - On npm scripts, I used npx, but since Tailwind is installed, it will not retrieve files anymore from npm and I can still compile the css via Tailwind CLI.
 
--[10/30/202] JIT functioning correctly but slow on the receiving of new classes.
+- [10/30/2021] JIT functioning correctly but slow on the receiving of new classes.
--- public/editor.js
+++ public/editor.js
@@ -1,56 +1,105 @@
-// editor behavior
+// DOM calls.
 const editor = document.querySelector(".editor");
 const liveView = document.querySelector(".liveView");
 const btn = document.querySelector("#runCMD");
 const loader = document.querySelector('#loader');
 const mainEditor = document.querySelector('.main-editor');
 const autoCompile = document.querySelector('#autoCompile');
+  // Retrieve tailwind from stylesheet and initially load it on the iframe.
+const tailwindCSS = [document.styleSheets[0]] 
+  .map(styleSheet => {
+    try {
+      return [...styleSheet.cssRules]
+        .map(rule => rule.cssText)
+        .join('');
+    } catch (e) {
+      console.log('Access to stylesheet %s is denied. Ignoring...', styleSheet.href);
+    }
+  })
+  .filter(Boolean)
+  .join('\n');
 
-// global variables
+// Global variables.
 var loaderTimer;
 var autoCompileBtn = true;
 
-// editor settings and configurations
+// Editor and preview window settings and configurations.
 const aceEditor = ace.edit("editor");
 aceEditor.setTheme("ace/theme/twilight");
 aceEditor.session.setMode("ace/mode/html");
 aceEditor.setFontSize(17);
-
-// RUN button
-btn.addEventListener('click', jitTailwind)
-
-// onkeyup event on the editor
-editor.addEventListener('keyup', ()=>{
-  liveView.innerHTML = aceEditor.getValue() 
-})
-
-// auto complie on keyup and after a set period
-editor.addEventListener('keyup', keyupListener)
-
-// paste event on the editor
-editor.addEventListener("paste", function(e) {
-  e.preventDefault();
-  var text = e.clipboardData.getData("text/plain");
-  document.execCommand("insertText", false, text);
-  jitTailwind();
+aceEditor.setOptions({
+  enableBasicAutocompletion: [{
+    getCompletions: (editor, session, pos, prefix, callback) => {
+  // Note, won't fire if caret is at a word that does not have these letters
+      callback(null, tailwindCompletions);
+    },
+  }],
+  // To make popup appear automatically, without explicit _ctrl+space_.
+  enableLiveAutocompletion: true,
 });
+  // Initial setting of iframe for page preview.
+liveView.setAttribute('srcdoc', `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Talwind Editor</title>
+  </head>
+  <style>
+  ${tailwindCSS}
+  </style>
+  <style id="customCSS">
+  </style>
+  <body>
 
-// user preference of auto compile or manual
+  </body>
+  </html>
+  `)
+
+// Load event Listeners on window load.
+window.onload = ()=>{
+  // RUN button
+  btn.addEventListener('click', jitTailwind)
+
+  // Onkeyup event on the editor.
+  editor.addEventListener('keyup', ()=>{
+    liveView.contentDocument.body.innerHTML = aceEditor.getValue();
+  })
+
+  // Auto compile on keyup and after a set period.
+  editor.addEventListener('keyup', keyupListener)
+
+  // Paste event on the editor.
+  editor.addEventListener("paste", function(e) {
+    e.preventDefault();
+    var text = e.clipboardData.getData("text/plain");
+    document.execCommand("insertText", false, text);
+    jitTailwind();
+  });
+}
+
+// User preference of auto compile or manual
 autoCompile.addEventListener('click', () => {
   if (autoCompileBtn){
   editor.removeEventListener('keyup', keyupListener);
-  autoCompile.innerText = 'Compile OFF'
+  clearTimeout(loaderTimer);
+  autoCompile.innerHTML = 'Compile <span class="buttonStatus">OFF</span>'
   autoCompileBtn = false;
   } else {
   editor.addEventListener('keyup', keyupListener);
-  autoCompile.innerText = 'Compile AUTO'
+  autoCompile.innerHTML = 'Compile <span class="buttonStatus">AUTO</span>'
   autoCompileBtn = true;   
   }
 })
 
-// request function for compiled custom css
+// Request function for compiled custom css.
 async function jitTailwind(){
   editor.removeEventListener('keyup', keyupListener)
+  btn.disabled = true;
+  autoCompile.disabled = true;
   loader.classList.toggle('hidden');
   let response = await fetch('/tailwind',{
     method: 'POST',
@@ -62,17 +111,34 @@ async function jitTailwind(){
     })
   let status = await response.json()
   if (status.tailwind === 'updated'){
-    document.getElementById('customCSS').innerHTML = decodeURI(status.css);
+    // Apply custom css to preview page
+    liveView.contentWindow.document.getElementById('customCSS').innerHTML = decodeURI(status.css);
   } else {
     alert('Sum ting whent wong ohn da sirvir. Pwease welowd de eyjitur. UwU')
   }
   liveView.innerHTML = aceEditor.getValue();
-  editor.addEventListener('keyup', keyupListener)
+  if(autoCompileBtn){
+    editor.addEventListener('keyup', keyupListener)
+  }
+  btn.disabled = false;
+  autoCompile.disabled = false;  
   loader.classList.toggle('hidden')
 }
 
-// Timer function for keyup event listener
+// Timer function for keyup event listener.
 function keyupListener(){  
   clearTimeout(loaderTimer);
   loaderTimer = setTimeout(jitTailwind, 5000);
 }
+
+// Tailwind completions.
+const tailwindCompletions =  [
+  {value: 'container', score: 1, meta: `max-width = min-width`},
+  {value: 'decoration-slice', score: 1, meta: `box-decoration-break: slice`},
+  {value: 'decoration-clone', score: 1, meta: `box-decoration-break: clone`},
+  {value: 'box-border', score: 1, meta: `box-sizing: border-box`},
+  {value: 'box-content', score: 1, meta: `box-sizing: content-box`},
+]
+
+
+  
