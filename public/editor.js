af81067f92754b45df61e2e2a9afc80f65fdcbc2
Daneil Benedict L. Omoto <dbomoto@gmail.com>
2022-02-03 14:01:02 +0000

    Added resize feature for editor.

--- public/editor.js
+++ public/editor.js
@@ -2,8 +2,18 @@
 const editor = document.querySelector("#editor");
 const liveView = document.querySelector("#liveView");
 const btn = document.querySelector("#runCMD");
+const newTab = document.getElementById('newTab');
+
+// Observer for resize of the editor
+const editorResize = new ResizeObserver(el=>{
+  aceEditor.setOptions({wrap:false})
+  aceEditor.setOptions({wrap:true}) 
+})
+editorResize.observe(editor);
+
 
 // Editor and preview window settings and configurations.
+ace.require("ace/ext/language_tools");
 const aceEditor = ace.edit("editor");
 aceEditor.setTheme("ace/theme/twilight");
 aceEditor.session.setMode("ace/mode/html");
@@ -12,6 +22,7 @@ aceEditor.setOptions({
   enableBasicAutocompletion: [{
     getCompletions: (editor, session, pos, prefix, callback) => {
   // Note, won't fire if caret is at a word that does not have these letters
+  // console.log(editor,session,pos,prefix)
       callback(null, tailwindCompletions);
     },
   }],
@@ -39,10 +50,15 @@ window.onload = ()=>{
   // Generate button 
   btn.addEventListener('click', retrieveCSS)
 
+  // Open in New Tab button
+  newTab.addEventListener('click', openNewTab)
+
   // Onkeyup while focus is on the Editor, automatically pass data to live view of webpage.
   editor.addEventListener('keyup', ()=>{
     liveView.contentDocument.body.innerHTML = aceEditor.getValue();
   })
+
+  // editor.addEventListener('re')
 
 }
 
@@ -52,3 +68,20 @@ function retrieveCSS() {
   console.log(data) // Show this data to client as output during Generate
 }
   
+//Opens the code in a new tab. 
+function openNewTab() {
+ window.open().document.write(`  
+ <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Talwind Editor</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+  </head>
+  <body>${aceEditor.getValue()}</body>
+  </html> `) 
+}
+
+
