ed7fdb6d3d15bd5f27d814a511186654aa58dbdf
Daneil Benedict L. Omoto <dbomoto@gmail.com>
2022-02-11 10:59:52 +0000

    Added the getCSS feature and partial completion of autocomplete window resizing.

--- public/editor.js
+++ public/editor.js
@@ -12,7 +12,7 @@ const editorResize = new ResizeObserver(el=>{
 editorResize.observe(editor);
 
 
-// Editor and preview window settings and configurations.
+// Editor settings and configurations.
 ace.require("ace/ext/language_tools");
 const aceEditor = ace.edit("editor");
 aceEditor.setTheme("ace/theme/twilight");
@@ -30,6 +30,16 @@ aceEditor.setOptions({
   enableLiveAutocompletion: true,
 });
 
+
+// Editor for Modal
+const genCSS = ace.edit("genCSS");
+genCSS.setTheme("ace/theme/twilight");
+genCSS.session.setMode("ace/mode/css");
+genCSS.setFontSize(17);
+genCSS.setOptions({wrap:true})
+
+
+
 // Initial set up of iframe for page preview.
 liveView.setAttribute('srcdoc', `
   <!DOCTYPE html>
@@ -44,6 +54,7 @@ liveView.setAttribute('srcdoc', `
   <body></body>
   </html>
   `)
+
 
 // Load initial event Listeners on window load.
 window.onload = ()=>{
@@ -62,13 +73,49 @@ window.onload = ()=>{
 
 }
 
-// Retrieves the generated css from Tailwind
+
+// Retrieves the generated css from Tailwind and display to user.
+let modalGenerate = document.querySelector('.modal-generate')
+let genCSSElement = document.querySelector('#genCSS')
 function retrieveCSS() {
-  let data = liveView.contentWindow.document.getElementsByTagName("style")[0].innerHTML;
-  console.log(data) // Show this data to client as output during Generate
+  btn.classList.add('cursor-wait','animate-pulse');
+  editor.classList.add('cursor-wait','animate-pulse');
+  aceEditor.setOptions({readOnly:true})
+  btn.disabled = true
+  // Removes unused arbitrary values from Tailwind.
+  liveView.setAttribute('srcdoc', `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+      <meta name="viewport" content="width=device-width,initial-scale=1">
+      <title>Talwind Editor</title>
+      <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body>${aceEditor.getValue()}</body>
+    <script>
+    window.onload = () => {
+      setTimeout(()=>{window.parent.passCSS()},5000)
+    }
+    </script>
+    </html>
+    `)
+
 }
+function passCSS(){
+  let data = liveView.contentWindow.document.getElementsByTagName("style")[0].innerHTML.replace('/* ! tailwindcss v3.0.18 | MIT License | https://tailwindcss.com */',"");
+  genCSS.session.setValue(vkbeautify.css(data,4))
+  modalGenerate.click()
+
+  btn.classList.remove('cursor-wait','animate-pulse')
+  editor.classList.remove('cursor-wait','animate-pulse');
+  aceEditor.setOptions({readOnly:false})
+  btn.disabled = false
+}
+
   
-//Opens the code in a new tab. 
+//Opens the page in a new tab. 
 function openNewTab() {
  window.open().document.write(`  
  <!DOCTYPE html>
@@ -83,5 +130,93 @@ function openNewTab() {
   <body>${aceEditor.getValue()}</body>
   </html> `) 
 }
+
+
+// Copy CSS to clipboard
+let copyNotifier = document.querySelector('#copy-notifier')
+function copyCSS() {
+
+  let css = genCSS.session.getValue();
+  navigator.clipboard.writeText(css);
+  copyNotifier.classList.toggle('hidden')
+  setTimeout(()=>{copyNotifier.classList.toggle('hidden')
+  }, 2000);
+}
+
+
+
+// AUTO ADJUST OF AUTOCOMPLETE WINDOW 
+
+
+
+
+// gets the current line where the cursor blinks
+// var currline = editor.getSelectionRange().start.row;
+// gets the code written on that line.
+// var wholelinetxt = editor.session.getLine(currline);
+// retrieves cursor position
+// aceEditor.getCursorPosition()
+
+
+// Change the width of these elements to adjust the autocomplete box
+//  ace_editor ace_autocomplete ace_dark ace-twilight
+// ace_scroller > ace_content
+
+// automcomplete suggustions is contained here
+// ace_layer ace_text-layer
+
+
+// Hypo
+// find the longest test currently used by the autompleter and set that as the longest Value
+
+// index 10-18 is where the autocomplete suggustions is placed 
+// retrieve the displayed automcomplete and store in an arraay
+var autocompleteArray = [];
+var longestValue = '';
+$('body').append("<span id='longText' class='hidden'></span>");
+var longElement = $('body').find("#longText");
+
+
+document.addEventListener('keydown',()=>{
+  var el = '';
+  for (let x=10; x<=18; x++){
+    let temp = 
+    $('.ace_layer,.ace_text-layer > .ace_line')[x].children[0].innerText +
+    $('.ace_layer,.ace_text-layer > .ace_line')[x].children[1].innerText + 
+    $('.ace_layer,.ace_text-layer > .ace_line')[x].children[2].innerText;
+    if (autocompleteArray.length > 9){
+      autocompleteArray.shift()
+      autocompleteArray.push(temp)
+    } else {
+      autocompleteArray.push(temp)
+    }
+  }
+
+  for(let temp of autocompleteArray){
+    if (temp.length >= longestValue.length){
+      longestValue = temp
+    }
+  }
+
+  longElement.text(longestValue); //longestValue containts the Long String
+  var longeElementWidth = longElement.width();
+  var autoCompleterWidth = $(".ace_autocomplete").width();
+
+      // var newAutoCompleteWidth = (longElement[0].innerText.length*4) + "px";
+
+      // $(".ace_editor.ace_autocomplete").width(longeElementWidth);  
+
+  if (longeElementWidth >= autoCompleterWidth) {
+      var newAutoCompleteWidth = longeElementWidth + 20 + "px";
+
+      $(".ace_editor.ace_autocomplete").width(newAutoCompleteWidth);
+  } else {
+      //Defaulting the width of the autocompleter to 350px
+      $(".ace_editor.ace_autocomplete").width("350px");
+  }     
+})
+
+
+
 
 
